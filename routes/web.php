928e67a83a5df0f5616656859ee19652cff47adb
bilal elhalawaty <bilal.halawaty@gmail.com>
2021-01-23 00:06:44 +0200

    fix issue posts controller wrong name

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-route::get('/','Postscontroller@index');
-route::get('/posts/create','Postscontroller@create');
-route::get('/posts/{post}','Postscontroller@show');
+route::get('/','PostsController@index');
+route::get('/posts/create','PostsController@create');
+route::get('/posts/{post}','PostsController@show');
 route::get('/posts/tags/{tags}','Tagscontroller@index');
-route::post('/posts','Postscontroller@store');
+route::post('/posts','PostsController@store');
 route::post('/post/{post}/comments','CommentsController@store');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Auth::routes();
